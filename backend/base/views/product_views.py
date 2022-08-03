45f488250c8e32ca59d22503d457c526fd69ab7a
Favour <favourgilead@gmail.com>
2022-08-03 19:24:11 +0100

    created signal for user and changed file arrangement

---
+++ backend/base/views/product_views.py
@@ -0,0 +1,21 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from base.models import Product
+from base.serializers import ProductSerializer
+from rest_framework import status
+
+# Create your views here
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(pk=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
